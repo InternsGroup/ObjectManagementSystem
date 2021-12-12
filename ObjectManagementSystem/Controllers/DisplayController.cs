d8ece45de568fb530df22a0284788b12c7bc7137
Muhammet Ertaş <e190503054@stud.tau.edu.tr>
2021-12-12 14:57:28 +0300

    categoryUpdate
    kategori bölümü eklendi, objelerde resimli seçenek için alternatif olarak tablo yapısı eklendi

--- ObjectManagementSystem/Controllers/DisplayController.cs
+++ ObjectManagementSystem/Controllers/DisplayController.cs
@@ -10,7 +10,8 @@ namespace ObjectManagementSystem.Controllers
     public class DisplayController : Controller
     {
         DB_STOREEntities db = new DB_STOREEntities();
-
+        public static int viewStatus = 1;
+        public static int categoryId = 0;
         [HttpGet]
         public ActionResult Index()
         {
@@ -18,11 +19,46 @@ namespace ObjectManagementSystem.Controllers
             {
                 ViewBag.myId = Session["ID"];
             }
+            ViewBag.viewStatus = viewStatus;
+            ViewBag.categories = db.CATEGORY_TABLE.ToList();
             ViewBag.reservedObjects = db.RESERVED_OBJECT_TABLE.ToList();
             ViewBag.loanedObjects = db.ACTION_TABLE.ToList();
             ViewBag.members = db.MEMBER_TABLE.ToList();
-            var objects = db.OBJECT_TABLE.ToList();
-            return View(objects);
+            if (TempData["objects"] == null)
+            {
+                categoryId = 0;
+                var allObjects = db.OBJECT_TABLE.ToList();
+                return View(allObjects);
+            }
+            else
+            {
+                var categorizedObjects = TempData["objects"];
+                ViewBag.categoryName = TempData["category"];
+                return View(categorizedObjects);
+            }
+        }
+
+        public ActionResult ChangeView()
+        {
+            try
+            {
+                if (categoryId != 0)
+                {
+                    TempData["objects"] = db.OBJECT_TABLE.Where(myObj => myObj.CATEGORY == categoryId).ToList();
+                    TempData["category"] = db.CATEGORY_TABLE.FirstOrDefault(myObj => myObj.ID == categoryId).NAME;
+                }
+            }
+            catch (Exception e) { }
+            viewStatus = viewStatus * -1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GetCategory(int id)
+        {
+            categoryId = id;
+            TempData["objects"] = db.OBJECT_TABLE.Where(myObj => myObj.CATEGORY == id).ToList();
+            TempData["category"] = db.CATEGORY_TABLE.FirstOrDefault(myObj => myObj.ID == id).NAME;
+            return RedirectToAction("Index");
         }
 
 
