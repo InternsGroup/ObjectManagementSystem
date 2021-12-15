523ef9617da0cff57553d8c8f13092db359b537d
Muhammet Ertaş <e190503054@stud.tau.edu.tr>
2021-12-15 23:20:50 +0300

    searchBar
    ana sayfaya arama cubugu eklendi, gerekli kodlar displaycontroller icine eklendi

--- ObjectManagementSystem/Controllers/DisplayController.cs
+++ ObjectManagementSystem/Controllers/DisplayController.cs
@@ -12,26 +12,57 @@ namespace ObjectManagementSystem.Controllers
         DB_STOREEntities db = new DB_STOREEntities();
         public static int viewStatus = 1;
         public static int categoryId = 0;
-        [HttpGet]
-        public ActionResult Index()
+
+        public ActionResult Index(string search)
         {
+            //kullanicinin girisini kontrol ediyor
             if (Session["Username"] != null)
             {
                 ViewBag.myId = Session["ID"];
             }
+            //sayfaya veri tabanindan gerekli bilgileri cekiyoruz
             ViewBag.viewStatus = viewStatus;
             ViewBag.categories = db.CATEGORY_TABLE.ToList();
             ViewBag.reservedObjects = db.RESERVED_OBJECT_TABLE.ToList();
             ViewBag.loanedObjects = db.ACTION_TABLE.ToList();
             ViewBag.members = db.MEMBER_TABLE.ToList();
+            //kategori secili degilse ve sayfa ilk yukleniyorsa if, kategori secilmis ise else calisir
+            //sayfa ilk acilirken tempdata bos bu yuzden if calisir ve tum objeler sayfaya gonderilir
+            //kategori secilince tempdata["objects"] icerisine kategorideki objeler kaydediliyor
             if (TempData["objects"] == null)
             {
-                categoryId = 0;
-                var allObjects = db.OBJECT_TABLE.ToList();
-                return View(allObjects);
+                IQueryable<OBJECT_TABLE> objects;
+
+                //kategori id 0 default yani tum objeleri ceker
+                if (categoryId == 0)
+                {
+                    objects = from allItems in db.OBJECT_TABLE select allItems;
+                }
+                else
+                {
+                    objects = from allItems in db.OBJECT_TABLE where allItems.CATEGORY == categoryId select allItems;
+                }
+                //arama yapilirsa metodun icine search degiskeni gelir ve null degilse aratilan objeler sayfaya gonderilir
+                if (!string.IsNullOrEmpty(search))
+                {
+                    objects = objects.Where(item => item.NAME.Contains(search));
+                    //sonuc bulunamazsa uyari gonderir
+                    if (objects.Count() == 0)
+                    {
+                        Session["message"] = "No items match your search...";
+                    }
+                }
+                try
+                {
+                    //kategori ismini sayfaya gonderiyoruz cunku kategori secildikten sonra tempdata bir sonraki islemde
+                    //kayboldugundan her anasayfada yapilan aratma veya goruntu degisikligi isleminde ismi tekrar gonderiyoruz
+                    ViewBag.categoryName = db.CATEGORY_TABLE.FirstOrDefault(myObj => myObj.ID == categoryId).NAME;
+                }catch(NullReferenceException e) { }
+                return View(objects.ToList());
             }
             else
             {
+                //kategoriyi menuden secerek acarsak burasi calisir ve tempdata uzerinden objeler alinir
                 var categorizedObjects = TempData["objects"];
                 ViewBag.categoryName = TempData["category"];
                 return View(categorizedObjects);
@@ -40,10 +71,12 @@ namespace ObjectManagementSystem.Controllers
 
         public ActionResult ChangeView()
         {
+            //sayfada listelenen objeler viewStatus uzerinden tablo seklinde veya modal popup seklinde gosterir
             try
             {
                 if (categoryId != 0)
                 {
+                    //sayfa kategori bolumunun birinde ise tempdata uzerinden hangi kategoride oldugunu indexe gondeririz
                     TempData["objects"] = db.OBJECT_TABLE.Where(myObj => myObj.CATEGORY == categoryId).ToList();
                     TempData["category"] = db.CATEGORY_TABLE.FirstOrDefault(myObj => myObj.ID == categoryId).NAME;
                 }
@@ -53,8 +86,17 @@ namespace ObjectManagementSystem.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult GoToMain()
+        {
+            //ana sayfa ikonuna tiklaninca kategori sayfasinin birinde ise default ana sayfaya doner
+            //ve tum objeler karisik sekilde listelenir
+            categoryId = 0;
+            return RedirectToAction("Index");
+        }
+
         public ActionResult GetCategory(int id)
         {
+            //secilen kategorinin id si class duzeyinde degiskene kayit edilir ve index ona gore hareket eder
             categoryId = id;
             TempData["objects"] = db.OBJECT_TABLE.Where(myObj => myObj.CATEGORY == id).ToList();
             TempData["category"] = db.CATEGORY_TABLE.FirstOrDefault(myObj => myObj.ID == id).NAME;
