576293d48b7bc8d51470c172877bf26d3db548d8
Muhammet Ertaş <e190503054@stud.tau.edu.tr>
2021-12-23 00:18:59 +0300

    register
    kayıt işleminde mevcut id ve email kontrolü sağlandı

--- ObjectManagementSystem/Controllers/RegisterController.cs
+++ ObjectManagementSystem/Controllers/RegisterController.cs
@@ -19,6 +19,24 @@ namespace ObjectManagementSystem.Controllers
         [HttpPost]
         public ActionResult Index(MEMBER_TABLE userObj)
         {
+            var userName = db.MEMBER_TABLE.FirstOrDefault(x => x.USERNAME == userObj.USERNAME);
+            var email = db.MEMBER_TABLE.FirstOrDefault(x => x.EMAIL == userObj.EMAIL);
+            ViewBag.Name = userObj.NAME;
+            ViewBag.Surname = userObj.SURNAME;
+            ViewBag.Email = userObj.EMAIL;
+            ViewBag.Username = userObj.USERNAME;
+            ViewBag.Password = userObj.PASSWORD;
+            ViewBag.Phone = userObj.TELNUMBER;
+            if (userName != null)
+            {
+                ViewBag.UsernameAlert = "This username is already in use. Please choose another username.";
+                return View(userObj);
+            }
+            if(email != null)
+            {
+                ViewBag.EmailAlert = "This email is already in use. Please choose another email.";
+                return View();
+            }
             db.MEMBER_TABLE.Add(userObj);
             db.SaveChanges();
             return RedirectToAction("Index", "LogIn");
